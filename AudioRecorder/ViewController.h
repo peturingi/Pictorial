62d7d201f559a4fe28031b9bd6950d670ae97b97
peturingi <petur@petur.eu>
2014-03-16 13:38:51 +0100

    Removed audio recorder as we will not be using it, removed CoreData logic which can be rewritten during refactoring.

--- AudioRecorder/ViewController.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-#import "AudioRecorderDelegate.h"
-
-@interface ViewController : UIViewController <AudioRecorderDelegate>
-@property (weak, nonatomic) IBOutlet UIButton *play;
-@property (weak, nonatomic) IBOutlet UIButton *stop;
-@property (weak, nonatomic) IBOutlet UIButton *record;
-
-@end
--- BBADataStack/BBADataStack/BBAContext.m
+++ BBADataStack/BBADataStack/BBAContext.m
@@ -29,17 +29,6 @@
     [_managedObjectContext deleteObject:anObject];
 }
 
--(void)saveAll{
-    BOOL hasChanges = [_managedObjectContext hasChanges];
-    if(hasChanges){
-        NSError *error = nil;
-        BOOL saveSuccess = [_managedObjectContext save:&error];
-        if(!saveSuccess){
-            [NSException raise:@"Save failed" format:@"The managed object context failed to perform a save. The generated error is: %@", error];
-        }
-    }
-}
-
 -(id)init{
     [NSException raise:@"-init not allowed" format:@"Initialization using standard -init is not allowed - please use the provided classmethod"];
     return nil;
