d12da870d4dade0979b88acf15eede3ac5527ba0
Brian Pedersen <brianfrostpedersen@gmail.com>
2014-03-21 13:23:58 +0100

    Fixed

--- Visual Scheduler/main.m
+++ Visual Scheduler/main.m
@@ -5,6 +5,22 @@
 int main(int argc, char * argv[]) {
     @autoreleasepool {
         [BBACoreDataStack installInMemory:YES];
+        
+#ifdef DEBUG
+        [[BBACoreDataStack sharedInstance] scheduleWithTitle:@"Monday" withBackgroundColour:0];
+        [[BBACoreDataStack sharedInstance] scheduleWithTitle:@"Visit Doctor Downtown" withBackgroundColour:0];
+        [[BBACoreDataStack sharedInstance] scheduleWithTitle:@"Day in the woods" withBackgroundColour:0];
+        [[BBACoreDataStack sharedInstance] scheduleWithTitle:@"Wednesday in the kindergarden" withBackgroundColour:0];
+        [[BBACoreDataStack sharedInstance] scheduleWithTitle:@"Monday the 24th of July 2014" withBackgroundColour:0];
+        [[BBACoreDataStack sharedInstance] scheduleWithTitle:@"Fredag" withBackgroundColour:0];
+        [[BBACoreDataStack sharedInstance] scheduleWithTitle:@"Mandag" withBackgroundColour:0];
+        [[BBACoreDataStack sharedInstance] scheduleWithTitle:@"Tirsdag" withBackgroundColour:0];
+        [[BBACoreDataStack sharedInstance] scheduleWithTitle:@"Onsdag" withBackgroundColour:0];
+        [[BBACoreDataStack sharedInstance] scheduleWithTitle:@"Torsdag" withBackgroundColour:0];
+        [[BBACoreDataStack sharedInstance] scheduleWithTitle:@"Fredag" withBackgroundColour:0];
+        [[[BBACoreDataStack sharedInstance] sharedManagedObjectContext] save:nil];
+#endif
+        
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
