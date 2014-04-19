b65ee37c1efcfffdf4c7be116e50e2a26d0eb572
Pétur Ingi Egilsson <petur@petur.eu>
2014-04-19 15:56:02 +0200

    Added select pictogram viewonctroller below the calendar view.

--- Visual Scheduler/CalendarView/ScheduleViewController.m
+++ Visual Scheduler/CalendarView/ScheduleViewController.m
@@ -5,5 +5,7 @@
     NSInteger selectedViewMode = ((UISegmentedControl *)sender).selectedSegmentIndex;
     [[NSNotificationCenter defaultCenter] postNotificationName:NOTIFICATION_CALENDAR_VIEW  object:[NSNumber numberWithInteger:selectedViewMode]];
 }
+- (IBAction)edit:(id)sender {
+}
 
 @end
