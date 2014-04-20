1175769d8bbf1b7849685bfb143acf9132fcbd70
Brian Pedersen <brianfrostpedersen@gmail.com>
2014-04-20 12:26:03 +0200

    Refactored NSString+CapitalizeSentence logic for readability

--- Visual Scheduler/Category/NSString+CapitalizeSentence.m
+++ Visual Scheduler/Category/NSString+CapitalizeSentence.m
@@ -10,6 +10,11 @@
 
 @implementation NSString (CapitalizeSentence)
 -(NSString*)capitalizedSentence{
-    return [self stringByReplacingCharactersInRange:NSMakeRange(0,1) withString:[[self substringToIndex:1] capitalizedString]];
+    if([self length] < 2){
+        return self.capitalizedString;
+    }
+    NSRange charactersInRange = NSMakeRange(0, 1);
+    NSString* firstCharacterCapitalized = [self substringToIndex:1].capitalizedString;
+    return [self stringByReplacingCharactersInRange:charactersInRange withString:firstCharacterCapitalized];
 }
 @end
