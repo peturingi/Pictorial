1b4ec8d9629f3f11bbe4408b579d9fb275034d11
Pétur Ingi Egilsson <petur@petur.eu>
2014-07-28 15:19:48 +0200

    Refactoring

--- ScheduleCollectionViewController.m
+++ ScheduleCollectionViewController.m
@@ -3,23 +3,53 @@
 
 @implementation ScheduleCollectionViewController
 
-- (IBAction)removePictogramFromSchedule:(UIGestureRecognizer *)sender
+#pragma mark - Remove pictogram from schedule
+
+/** Removes the pressed pictogram from the schedule and collection view it belonged to; persists the changes.
+ */
+- (IBAction)handleRemovalOfPictogramFromScheduleGesture:(UIGestureRecognizer *)sender
 {
+    NSAssert(sender, @"This method depends on the sender parameter.");
     if (sender.state == UIGestureRecognizerStateBegan) {
-        NSIndexPath * const pathOfPictogramToRemove = [self.collectionView indexPathForItemAtPoint:[sender locationInView:sender.view]];
-        if (pathOfPictogramToRemove) {
-            const id <NSFetchedResultsSectionInfo> section = [self.dataSource.fetchedResultsController sections][pathOfPictogramToRemove.section];
-            NSAssert([section objects].count == 1, @"Each day (section) must contain only a single schedule.");
-            const NSManagedObject *schedule = [section objects].firstObject;
-    
-            // Get the pictogram I want to display.
-            NSMutableArray *pictogramsInSchedule = [schedule valueForKey:CD_KEY_SCHEDULE_PICTOGRAMS];
-            [pictogramsInSchedule removeObjectAtIndex:pathOfPictogramToRemove.item];
-            [schedule setValue:pictogramsInSchedule forKeyPath:CD_KEY_SCHEDULE_PICTOGRAMS];
-            // TODO save the context.
-            [self.collectionView deleteItemsAtIndexPaths:@[pathOfPictogramToRemove]];
+        NSIndexPath * const pictogramToRemove = [self.collectionView indexPathForItemAtPoint:[sender locationInView:sender.view]];
+        if (pictogramToRemove) {
+            [self removePictogramAtIndex:pictogramToRemove.item fromSchedule:[self scheduleInSection:pictogramToRemove.section]];
+            [self saveSchedule];
+            [self.collectionView deleteItemsAtIndexPaths:@[pictogramToRemove]];
         }
     }
 }
+
+/** A section in the collection view represents a single schedule.
+ @pre The specified section contains one schedule.
+ */
+- (NSManagedObject *)scheduleInSection:(NSUInteger)section
+{
+    id <NSFetchedResultsSectionInfo> const sectionContainingSchedule = self.dataSource.fetchedResultsController.sections[section];
+    NSAssert([sectionContainingSchedule objects].count == 1, @"There should be a single schedule per section!");
+    return [sectionContainingSchedule objects].firstObject;
+}
+
+/** Removes a pictogram from the schedule.
+ @pre A pictogram exists in the schedule at the given index.
+ */
+- (void)removePictogramAtIndex:(NSUInteger)index fromSchedule:(NSManagedObject *)schedule
+{
+    NSMutableArray *pictogramsInSchedule = [schedule valueForKey:CD_KEY_SCHEDULE_PICTOGRAMS];
+    [pictogramsInSchedule removeObjectAtIndex:index];
+    [schedule setValue:pictogramsInSchedule forKeyPath:CD_KEY_SCHEDULE_PICTOGRAMS];
+}
+
+/** Persists changes made to the managed object context.
+ @throw NSException if the managed object context can not be saved.
+ */
+- (void)saveSchedule
+{
+    NSError *error;
+    [self.dataSource.managedObjectContext save:&error];
+    if (error) @throw [NSException exceptionWithName:@"Error saving deletion from schedule." reason:error.localizedFailureReason userInfo:nil];
+}
+
+#pragma mark -
 
 @end
