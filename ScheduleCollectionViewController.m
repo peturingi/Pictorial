921aed55b0933fc4593e51f0fa0df4634068a136
Pétur Ingi Egilsson <petur@petur.eu>
2014-08-01 12:52:52 +0200

    Eliminated a bug where if a pictogram was dropped in space between cells, it would not get added to a schedule.

--- ScheduleCollectionViewController.m
+++ ScheduleCollectionViewController.m
@@ -1,16 +1,18 @@
 #import "ScheduleCollectionViewController.h"
 #import <CoreData/CoreData.h>
+#import "CalendarCell.h"
 
 @implementation ScheduleCollectionViewController
 
-#pragma mark - Remove pictogram from schedule
+#pragma mark -
+#pragma mark Remove pictogram from schedule
 
-- (IBAction)deletePictogramFromSchedule:(UIButton *)sender
+- (IBAction)removePictogramFromSchedule:(UIButton *)sender
 {
     UICollectionViewCell * const sendersCell = (UICollectionViewCell *)sender.superview.superview;
     NSIndexPath * const pictogramToRemove = [self.collectionView indexPathForCell:sendersCell];
     if (pictogramToRemove) {
-        [self removePictogramAtIndex:pictogramToRemove.item fromSchedule:[self scheduleInSection:pictogramToRemove.section]];
+        [self removePictogramAtIndex:pictogramToRemove.item fromSchedule:[self scheduleForSection:pictogramToRemove.section]];
         [self saveSchedule];
         [self.collectionView deleteItemsAtIndexPaths:@[pictogramToRemove]];
     }
@@ -19,7 +21,7 @@
 /** A section in the collection view represents a single schedule.
  @pre The specified section contains one schedule.
  */
-- (NSManagedObject *)scheduleInSection:(NSUInteger)section
+- (NSManagedObject *)scheduleForSection:(NSUInteger)section
 {
     id <NSFetchedResultsSectionInfo> const sectionContainingSchedule = self.dataSource.fetchedResultsController.sections[section];
     NSAssert([sectionContainingSchedule objects].count == 1, @"There should be a single schedule per section!");
@@ -32,37 +34,90 @@
 - (void)removePictogramAtIndex:(NSUInteger)index
                   fromSchedule:(NSManagedObject *)schedule
 {
+    NSAssert(schedule, @"Expected a schedule.");
     [[schedule valueForKey:CD_KEY_SCHEDULE_PICTOGRAMS] removeObjectAtIndex:index];
 }
 
-/** Persists changes made to the managed object context.
- @throw NSException if the managed object context can not be saved.
- */
-- (void)saveSchedule
-{
-    NSAssert(self.dataSource.managedObjectContext, @"The data source does not have a managed object context.");
-    if ([self.dataSource.managedObjectContext hasChanges] == NO) return;
-    
-    NSError *error;
-    if ([self.dataSource.managedObjectContext save:&error] == NO) {
-        @throw [NSException exceptionWithName:@"Error saving deletion from schedule." reason:error.localizedFailureReason userInfo:nil];
-    }
-}
+#pragma mark -
+#pragma  mark Add pictogram to schedule
 
 - (BOOL)addPictogramWithID:(NSManagedObjectID *const)objectID
                    atPoint:(const CGPoint)point
             relativeToView:(UIView *)view
 {
-    NSAssert(objectID, @"objectID must not be nil.");
-    NSIndexPath * const target = [self.collectionView indexPathForItemAtPoint:[self.collectionView convertPoint:point fromView:view]];
-    if (target) {
-        NSManagedObject * const targetSection = [self scheduleInSection:target.section];
-        [self insertPictogramWithID:objectID inSchedule:targetSection atIndexPath:target];
-    } else return NO;
+    NSAssert(objectID, @"Expected objectID.");
+    NSAssert(view, @"Expected a relative view.");
+    
+    /*
+     (1)
+     If point is on a pictogram:
+        If point is above vertical center of pictogram, add new pictogram above
+        If point s below vertical center of pictogram, add new pictogram below
+     (2)
+     Else
+        X := pictogram with largest interserction area, intersecting the released pictogram
+        if X is not empty
+            if point is above vertical center of x, add new pictogram above X
+            if point is below vertical center of pictogram, add new pictogram below
+     */
+    
+    // (1)
+    NSIndexPath *pictogramAtDropPoint = [self.collectionView indexPathForItemAtPoint:[self.collectionView convertPoint:point fromView:view]]; // target := nil, if release was not on a pictogram
+    /* Offset the final location by one if the pictogram was dropped on a target, but below its vertical center. */
+    if (pictogramAtDropPoint) {
+        // Offset by one if below center
+        CGPoint const dropLocation = [self.collectionView convertPoint:point fromView:view];
+        CGPoint const centerOfCellAtDropLocation = [self.collectionView cellForItemAtIndexPath:pictogramAtDropPoint].center;
+        NSInteger offset = (centerOfCellAtDropLocation.y - dropLocation.y < 0) ? 1 : 0;
+        NSIndexPath * const destination = [NSIndexPath indexPathForItem:(pictogramAtDropPoint.item + offset) inSection:pictogramAtDropPoint.section];
+        
+        [self insertPictogramWithID:objectID inSchedule:[self scheduleForSection:pictogramAtDropPoint.section] atIndexPath:destination];
+    }
+    // (2)
+    else {
+        /* Get all CalendarCell from the collection view. */
+        NSMutableArray * const calendarCells = [[NSMutableArray alloc] initWithCapacity:self.collectionView.subviews.count];
+        for (CalendarCell *cell in self.collectionView.subviews ) {
+            if ([cell isMemberOfClass:[CalendarCell class]]) [calendarCells addObject:cell];
+        }
+        /* Find all cells intersecting the dragged pictogram. */
+        CGRect draggedRect = CGRectMake([self.collectionView convertPoint:point fromView:view].x - PICTOGRAM_SIZE_WHILE_DRAGGING / 2,
+                                        [self.collectionView convertPoint:point fromView:view].y - PICTOGRAM_SIZE_WHILE_DRAGGING / 2,
+                                        PICTOGRAM_SIZE_WHILE_DRAGGING,
+                                        PICTOGRAM_SIZE_WHILE_DRAGGING);
+        NSMutableArray *intersectingCells = [[NSMutableArray alloc] init];
+        for (CalendarCell *cell in calendarCells) {
+            if (CGRectIntersectsRect(cell.frame, draggedRect)) {
+                [intersectingCells addObject:cell];
+            }
+        }
+        /* Find the largest intersecting rect */
+        CGRect largestIntersectingRect = CGRectZero;
+        for (CalendarCell *cell in intersectingCells) {
+            CGRect intersection = CGRectIntersection(cell.frame, draggedRect);
+            if (intersection.size.height * intersection.size.width > largestIntersectingRect.size.height * largestIntersectingRect.size.width) {
+                largestIntersectingRect = intersection;
+            }
+        }
+        /* Get the pictogram currently occupying the area containing the largest intersecting rect. */
+        NSIndexPath * const pathToPictogramAtDestination = [self.collectionView indexPathForItemAtPoint:largestIntersectingRect.origin];
+        /* Offset by one if below the center */
+        CGPoint const centerOfPictogramAtDestination = [self.collectionView cellForItemAtIndexPath:pathToPictogramAtDestination].center;
+        NSInteger const offset = (centerOfPictogramAtDestination.y - [self.collectionView convertPoint:point fromView:view].y < 0) ? 1 : 0;
+        NSIndexPath * const destination = [NSIndexPath indexPathForItem:(pathToPictogramAtDestination.item + offset) inSection:pathToPictogramAtDestination.section];
+        
+        [self insertPictogramWithID:objectID inSchedule:[self scheduleForSection:pathToPictogramAtDestination.section] atIndexPath:destination];
+    }
     
     if (NO == [self.dataSource.managedObjectContext save:nil]) return NO;
-    
-    return YES;
+    else return YES;
+}
+
+- (CGFloat)distanceBetweenPoint:(CGPoint)A andPoint:(CGPoint)B {
+    CGFloat aPower2 = powf(A.x, 2) - powf(B.x, 2);
+    CGFloat bPower2 = powf(A.y, 2) - powf(B.y, 2);
+    CGFloat cPower2 = aPower2 + bPower2;
+    return sqrtf(cPower2);
 }
 
 - (void)insertPictogramWithID:(NSManagedObjectID * const)objectID
@@ -78,11 +133,28 @@
     [self.collectionView insertItemsAtIndexPaths:@[indexPath]];
 }
 
+#pragma mark -
+
 /** Convinience method
  */
 - (NSManagedObjectContext *)managedObjectContext
 {
     return self.dataSource.managedObjectContext;
+}
+
+
+/** Persists changes made to the managed object context.
+ @throw NSException if the managed object context can not be saved.
+ */
+- (void)saveSchedule
+{
+    NSAssert(self.dataSource.managedObjectContext, @"The data source does not have a managed object context.");
+    if ([self.dataSource.managedObjectContext hasChanges] == NO) return;
+    
+    NSError *error;
+    if ([self.dataSource.managedObjectContext save:&error] == NO) {
+        @throw [NSException exceptionWithName:@"Error saving deletion from schedule." reason:error.localizedFailureReason userInfo:nil];
+    }
 }
 
 @end
