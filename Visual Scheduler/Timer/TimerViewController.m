36537bfc196a86ae4468014c8916e6f17c6b0bdb
Pétur Ingi Egilsson <petur@petur.eu>
2014-08-16 15:49:04 +0200

    todo syntax

--- Visual Scheduler/Timer/TimerViewController.m
+++ Visual Scheduler/Timer/TimerViewController.m
@@ -54,7 +54,7 @@
 }
 
 -(void)setupTimerView{
-    _timerView = [[TimerView alloc]initWithFrame:self.view.frame]; // TODO try self.view.frame þpetur
+    _timerView = [[TimerView alloc]initWithFrame:self.view.frame]; // TODO: try self.view.frame þpetur
     [_timerView setLabelToUpdate:_titleLabel];
     [[self view] addSubview:_timerView];
 }
